d6cc7b1e31d704ab3b6205e85c33ad281dc8bd25
El Mahdi Benbrahim <benbrahim.elmahdi@gmail.com>
2022-05-04 16:21:53 +0200

    [UPDATE] Injection of Header & Footer

--- public/assets/js/cookies.js
+++ public/assets/js/cookies.js
@@ -3,7 +3,7 @@ let getCompanyDetails = async () => {
     return await company.json();
 }
 
-window.addEventListener("DOMContentLoaded", async (event) => {
+window.addEventListener("DOMContentLoaded", async (event) => { 
     getCompanyDetails().then(result => {
         let b = Array.prototype.slice.call(document.getElementsByClassName('company'));
         b.forEach(item => {
